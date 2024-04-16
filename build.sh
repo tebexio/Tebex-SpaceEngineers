993df61fe4fa84a537893deadd420f539a7f6058
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-04-16 09:43:45 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Uses msbuild to compile the release version of the plugin. A distributable zip will be placed in your working dir.
+VERSION="2.0.0"
+workingDir=$(pwd)
+msbuild Tebex-SpaceEngineers.sln -p:Configuration=Release
+cd ./Tebex-SpaceEngineers/bin/x64/Release
+mv Tebex-SpaceEngineers.dll Tebex-SpaceEngineers-${VERSION}.dll
+zip "Tebex-SpaceEngineers-${VERSION}.zip" 0Harmony.dll Tebex-SpaceEngineers-${VERSION}.dll
+mv "Tebex-SpaceEngineers-${VERSION}.zip" "${workingDir}"
+cd $workingDir
