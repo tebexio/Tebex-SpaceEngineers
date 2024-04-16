aa1e38464ff295b67e0fa814173b7febdd0260e9
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-04-16 14:50:41 -0500

    resolve issues with giving items

--- Tebex-SpaceEngineers/SpaceEngineersCommands.cs
+++ Tebex-SpaceEngineers/SpaceEngineersCommands.cs
@@ -32,71 +32,78 @@ namespace TebexSpaceEngineersPlugin
             foreach (var definition in publicItems)
             {
                 var defIdentifier = definition.ToString().Replace("MyObjectBuilder_", ""); // definition.Id.ToString() = "MyObjectBuilder_Ore/Gold"
-                adapter.LogDebug($"caching item definition '{defIdentifier}'");
+                adapter.LogDebug($"found item definition - '{defIdentifier}'");
                 _itemDefinitions.Add(defIdentifier, definition);
             }
         }
         
         public static bool GiveItem(BaseTebexAdapter adapter, MyPlayer player, string itemName, uint quantity)
         {
-            if (player == null)
+            try
             {
-                adapter.LogError($"Failed to get player for give item command");
-                return false;
+                if (player == null)
+                {
+                    adapter.LogError($"Failed to get player for give item command");
+                    return false;
+                }
+
+                if (!_itemDefinitions.ContainsKey(itemName))
+                {
+                    adapter.LogError($"Item ID not found: '{itemName}'");
+                    return false;
+                }
+
+                var itemDef = _itemDefinitions[itemName];
+                var itemOb = MyObjectBuilderSerializer.CreateNewObject(itemDef.Id.TypeId, itemDef.Id.SubtypeName);
+                if (itemOb == null)
+                {
+                    adapter.LogError($"Failed to create object builder");
+                    return false;
+                }
+
+                var item = itemOb as MyObjectBuilder_PhysicalObject;
+                if (item == null)
+                {
+                    adapter.LogError($"Failed to create physical object for {itemName}");
+                    return false;
+                }
+
+                // itemName = "Ingot/Iron"
+                item.SubtypeName = itemName.Split('/')[1]; // subtype name must be target item name "Iron"
+                var inventory = player.Character.GetInventoryBase() as MyInventory;
+                if (inventory == null)
+                {
+                    adapter.LogError("No inventory obj for player " + player.Character.Name);
+                    return false;
+                }
+
+                var maxVolume = inventory.MaxVolume;
+
+                var intQty = (int)quantity;
+                IMyInventoryItem invItem = new MyPhysicalInventoryItem(intQty, item);
+                inventory.ResetVolume();
+                bool inventoryAddSuccess = inventory.Add(invItem, intQty);
+                if (!inventoryAddSuccess)
+                {
+                    adapter.LogError($"Failed to add {intQty}x of item {itemName} to player {player.DisplayName}");
+                    return false;
+                }
+
+                inventory.FixInventoryVolume((float)maxVolume);
+                return true;
+            }
+            catch (Exception e)
+            {
+                adapter.LogDebug($"Unexpected exception while giving item: {e.Message}");
+                adapter.LogDebug(e.StackTrace);
             }
 
-            if (!_itemDefinitions.ContainsKey(itemName))
-            {
-                adapter.LogError($"Item ID not found: '{itemName}'");
-                return false;
-            }
-            
-            var itemDef = _itemDefinitions[itemName];
-            var itemOb = MyObjectBuilderSerializer.CreateNewObject(itemDef.Id.TypeId, itemDef.Id.SubtypeName);
-            if (itemOb == null)
-            {
-                adapter.LogError($"Failed to create object builder");
-                return false;
-            }
-
-            var item = itemOb as MyObjectBuilder_PhysicalObject;
-            if (item == null)
-            {
-                adapter.LogError($"Failed to create physical object for {itemName}");
-                return false;
-            }
-
-            item.SubtypeName = itemName;
-            var inventory = player.Character.GetInventoryBase() as MyInventory;
-            if (inventory == null)
-            {
-                adapter.LogError("No inventory obj for player " + player.Character.Name);
-                return false;
-            }
-
-            var maxVolume = inventory.MaxVolume;
-            var qtyFixedPoint = new MyFixedPoint();
-            qtyFixedPoint.RawValue = quantity;
-            
-            IMyInventoryItem invItem = new MyPhysicalInventoryItem(qtyFixedPoint, item);
-            inventory.ResetVolume();
-            bool inventoryAddSuccess = inventory.Add(invItem, qtyFixedPoint);
-            if (!inventoryAddSuccess)
-            {
-                adapter.LogError($"Failed to add {qtyFixedPoint}x of item {itemName} to player {player.DisplayName}");
-                return false;
-            }
-            
-            inventory.FixInventoryVolume((float)maxVolume);
-            return true;
+            return false;
         }
 
         public static bool GiveSpaceCredits(BaseTebexAdapter adapter, MyPlayer player, uint amount)
         {
-            long balanceBefore = MyBankingSystem.GetBalance(player.Identity.IdentityId);
-            bool success = MyBankingSystem.ChangeBalance(player.Identity.IdentityId, amount);
-           
-            return success;
+            return MyBankingSystem.ChangeBalance(player.Identity.IdentityId, amount);
         }
     }
 }
